1342dbc27dfdf5876621102d66b9507e2fcaa9fc
Chris Harvey <chrisharvey2pi@gmail.com>
2018-02-18 20:50:19 -0500

    add c-Permalink src files

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
+const xjs = require('extrajs-dom')
+
 module.exports = {
+  xPermalink: xjs.HTMLTemplateElement.fromFileSync(path.join(__dirname, './tpl/x-permalink.tpl.html')).setRenderer(require('./tpl/x-permalink.tpl.js')),
 }
