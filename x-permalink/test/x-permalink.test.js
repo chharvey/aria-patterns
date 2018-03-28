748f25080ceca6ed59100258ba4ca7a9a1911722
Chris Harvey <chrisharvey2pi@gmail.com>
2018-03-27 22:10:28 -0400

    fixup permalink docs & styles

--- x-permalink/test/x-permalink.test.js
+++ x-permalink/test/x-permalink.test.js
@@ -1,9 +1,19 @@
+const fs = require('fs')
+const path = require('path')
+
+const xjs = require('extrajs-dom')
+
+const createDir = require('../../lib/createDir.js')
 const ARIAPatterns = require('../../index.js')
 
-console.log(ARIAPatterns.xPermalink.render({ id: 'my_id' }).querySelector('a').outerHTML)
 
-console.log(ARIAPatterns.xPermalink.render({
-  id   : 'my_id',
-  text : 'custom text',
-  label: 'custom label',
-}).querySelector('a').outerHTML)
+let output = `
+<!--link rel="stylesheet" href="/node_modules/aria-patterns/x-permalink/css/dist/c-Permalink.css"/-->
+<section id="section-title">
+  <h1>Section Title${new xjs.DocumentFragment(ARIAPatterns.xPermalink.render({ id: 'section-title' })).innerHTML()}</h1>
+</section>
+`
+
+createDir('./x-permalink/test/out/').then(function (v) {
+  fs.writeFileSync(path.resolve(__dirname, './out/x-permalink.test.html'), output, 'utf8')
+})
