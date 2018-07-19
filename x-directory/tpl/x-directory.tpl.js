d50179aeea34a76192c43d60b2ec5d922a1017f8
Chris Harvey <chrisharvey2pi@gmail.com>
2018-07-18 21:56:31 -0400

    add opts param to xDirectory_renderer

--- x-directory/tpl/x-directory.tpl.js
+++ x-directory/tpl/x-directory.tpl.js
@@ -10,17 +10,19 @@ const xjs = {
  * @param   {DocumentFragment} frag the template content with which to render
  * @param   {sdo.WebPage} data                              http://schema.org/WebPage
  * @param   {(sdo.WebPage|Array<sdo.WebPage>)} data.hasPart http://schema.org/hasPart
- * @param   {integer=} [data.$depth=Infinity] number of nested directory levels
+ * @param   {integer=} [data.$depth=Infinity] deprecated form of param `options.depth`
+ * @param   {!Object=} opts additional rendering options
+ * @param   {integer=} [opts.depth=Infinity] number of nested directory levels
  */
-function xDirectory_renderer(frag, data) {
+function xDirectory_renderer(frag, data, opts = {}) {
   let subpages = (xjs.Object.typeOf(data.hasPart) === 'array' ) ? data.hasPart : [data.hasPart]
-  let depth    = (xjs.Object.typeOf(data.$depth)  === 'number') ? data.$depth  : Infinity
-  new xjs.HTMLOListElement(frag.querySelector('[role="directory"]')).populate(subpages, function (f, d) {
+  let depth    = (xjs.Object.typeOf(opts.depth)   === 'number') ? opts.depth : (xjs.Object.typeOf(data.$depth)  === 'number') ? data.$depth : Infinity
+  new xjs.HTMLOListElement(frag.querySelector('[role="directory"]')).populate(subpages, function (f, d, o) {
     f.querySelector('[itemprop="url"]' ).href        = d.url
     f.querySelector('[itemprop="name"]').textContent = d.name
     if (d.hasPart && depth > 0) {
       f.querySelector('[itemprop="hasPart"]').append(
-        require(__filename).render({ ...d, $depth: depth - 1 })
+        require(__filename).render(d, null, { depth: depth - 1 })
       )
     }
   })
